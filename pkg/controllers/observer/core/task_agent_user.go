28211d504812a538bea4769ef6e7f381e7fad462
alv00 <dingjing0604@outlook.com>
2022-06-22 11:54:10 +0800

    config json

--- pkg/controllers/observer/core/task_agent_user.go
+++ pkg/controllers/observer/core/task_agent_user.go
@@ -13,6 +13,8 @@ See the Mulan PSL v2 for more details.
 package core
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	cloudv1 "github.com/oceanbase/ob-operator/apis/cloud/v1"
 	observerconst "github.com/oceanbase/ob-operator/pkg/controllers/observer/const"
@@ -20,7 +22,6 @@ import (
 	"io/ioutil"
 	"k8s.io/klog/v2"
 	"net/http"
-	"strings"
 )
 
 const (
@@ -28,9 +29,26 @@ const (
 	TraceIdKey       = "traceId"
 )
 
+type Config struct {
+	User        string `json:"user"`
+	Password    string `json:"password"`
+	HostIp      string `json:"hostIp"`
+	ClusterName string `json:"clusterName"`
+	ClusterId   string `json:"clusterId"`
+	ZoneName    string `json:"zoneName"`
+}
+
 func (ctrl *OBClusterCtrl) CreateUserForObagent(statefulApp cloudv1.StatefulApp) error {
 	subsets := statefulApp.Status.Subsets
+	// 获得所有的 obagent
+	klog.Infoln("try to get obagent", subsets)
+	klog.Infoln("try to get obagent", subsets[0])
+	klog.Infoln("try to get obagent", subsets[0].Pods[0])
+	klog.Infoln("try to get obagent", subsets[0].Pods[1])
+	klog.Infoln("try to get obagent", subsets[0].Pods[0].PodIP)
+	klog.Infoln("try to get obagent", subsets[0].Pods[1].PodIP)
 	podIp := subsets[0].Pods[0].PodIP
+
 	err := sql.CreateUser(podIp, "ocp_monitor", "root")
 	if err != nil {
 		return err
@@ -44,40 +62,35 @@ func (ctrl *OBClusterCtrl) CreateUserForObagent(statefulApp cloudv1.StatefulApp)
 }
 
 func (ctrl *OBClusterCtrl) ReviseConfig(podIp string) {
-	client := &http.Client{}
-	var data = strings.NewReader(SetConfig())
+	config := Config{
+		User:        "ocp_monitor",
+		Password:    "root",
+		HostIp:      podIp,
+		ClusterName: "ob-test",
+		ClusterId:   "1",
+		ZoneName:    "zone1",
+	}
 
 	updateUrl := fmt.Sprintf("http://%s:%d%s", podIp, observerconst.MonagentPort, observerconst.MonagentUpdateUrl)
-	req, err := http.NewRequest("POST", updateUrl, data)
+	body, _ := json.Marshal(config)
+	resp, err := http.Post(updateUrl, "application/json", bytes.NewBuffer(body))
+
 	if err != nil {
-		klog.Errorln("ger new request", err)
-	}
-	req.Header.Set("content-type", "application/json")
-	resp, err := client.Do(req)
-	if err != nil {
-		klog.Errorln("client do", err)
+		panic(err)
 	}
 	defer resp.Body.Close()
-	bodyText, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		klog.Errorln("read all response body", err)
+
+	if resp.StatusCode == http.StatusCreated {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			//Failed to read response.
+			panic(err)
+			klog.Errorln("Fail to read response:", err)
+		}
+		jsonStr := string(body)
+		klog.Errorln("Update config Response: ", jsonStr)
+	} else {
+		//The status is not Created. print the error.
+		klog.Errorln("Get failed with error: ", resp.Status)
 	}
-	klog.Infoln("response body :", bodyText)
-}
-
-//curl -X POST                                                                          -X/--request |指定什么命令|
-//-H 'content-type:application/json'													-H/--header 自定义头信息传递给服务器
-//-d '{"configs":[{"key":"monagent.pipeline.ob.status", "value":"active"}]}'            -d/--data HTTP POST方式传送数据
-//-L 'http://127.1:8088/api/v1/module/config/update
-
-func SetConfig() string {
-	config := `{"configs":[ 
-							{"key":"monagent.ob.monitor.user", "value":"ocp_monitor"},
-							{"key":"monagent.ob.monitor.password", "value":"root"},
-							{"key":"monagent.host.ip", "value":"10.42.0.178"},
-							{"key":"monagent.ob.cluster.name", "value":"ob-test"},
-							{"key":"monagent.ob.cluster.id", "value":"1"},
-							{"key":"monagent.ob.zone.name", "value":"zone1"},
-				]}`
-	return config
 }
