576d3a69c322b715946620973401bfb456612536
alv00 <dingjing0604@outlook.com>
2022-06-30 14:18:01 +0800

    conf

--- pkg/controllers/observer/const/statefulapp.go
+++ pkg/controllers/observer/const/statefulapp.go
@@ -29,6 +29,6 @@ const (
 
 	// monagent
 	MonagentConfigPeriod = 2
-	ConfFileStorageName  = "obagent conf file"
+	ConfFileStorageName  = "obagent-conf-file"
 	ConfFileStoragePath  = "/home/admin/obagent/conf"
 )
